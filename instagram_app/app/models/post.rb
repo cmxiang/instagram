1b15d53c9bb015ff605c6b21bc5765c0b0eba058
crystal xiang <cmxiang88@gmail.com>
2017-02-10 07:11:57 -0800

    added instagram_app

---
+++ instagram_app/app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+	has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "/image/:style/missing.png" 
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+end
